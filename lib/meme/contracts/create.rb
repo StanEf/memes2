a8b554a0c1e56ff69f7506c07284684f7c4d09cb
StanEf <stefremov@yandex.ru>
2020-10-30 03:43:03 -0700

    Добавляет валидации в контракт для создания мема

--- lib/meme/contracts/create.rb
+++ lib/meme/contracts/create.rb
@@ -1,31 +1,3 @@
-module Meme
-  module Contracts
-    class Create < ApplicationContract
-      params do
-        required(:template_id).value(:integer)
-        optional(:text0).value(:string)
-        optional(:text1).value(:string)
-        optional(:font).value(:integer)
-        optional(:max_font_size).value(:integer)
-
-        #todo тут надо сделать валидацию если templates.box_count > 2 то поле обязательно
-        #todo и boxes.count == templates.box_count
-        # надо еще прокидывать сюда параметры из шаблона
-        optional(:boxes).value(:array, min_size?: 0).each do
-          hash do
-            required(:text).filled(:string)
-            required(:x).filled(:integer)
-            required(:y).filled(:integer)
-            required(:width).filled(:integer)
-            required(:height).filled(:integer)
-            required(:color).filled(Types::Color)
-            required(:outline_color).filled(Types::Color)
-          end
-        end
-      end
-    end
-  end
-end
 # params =
 #   {
 #       template_id: 123123,
@@ -56,3 +28,71 @@ end
 #   }
 # contract = Meme::Contracts::Create.new
 # contract.call(params).errors.to_h
+module Meme
+  module Contracts
+    class Create < ApplicationContract
+      params do
+        required(:template).value(:integer)
+        required(:box_count).value(:integer)
+
+        optional(:text0).value(:string)
+        optional(:text1).value(:string)
+        optional(:font).value(:integer)
+        optional(:max_font_size).value(:integer)
+
+        #todo тут надо сделать валидацию если templates.box_count > 2 то поле обязательно
+        #todo и boxes.count == templates.box_count
+        # надо еще прокидывать сюда параметры из шаблона
+        optional(:boxes).value(:array, min_size?: 0).each do
+          hash do
+            required(:text).filled(:string)
+            required(:x).filled(:integer)
+            required(:y).filled(:integer)
+            required(:width).filled(:integer)
+            required(:height).filled(:integer)
+            required(:color).filled(Types::Color)
+            required(:outline_color).filled(Types::Color)
+          end
+        end
+      end
+
+      rule(:text0, :boxes) do
+        key.failure('must not be transmitted simultaneously') if key?(:boxes) && key?(:text0)
+      end
+
+      rule(:text1, :boxes) do
+        key.failure('must not be transmitted simultaneously') if key?(:boxes) && key?(:text1)
+      end
+
+      rule(:text0, :box_count, :boxes, ) do
+        if key?(:text0) && !key?(:boxes) && [1, 2].include?(values[:box_count])
+          key.failure('text0 must be exist')
+        end
+      end
+
+      rule(:text1, :box_count, :boxes, ) do
+        if key?(:text1) && !key?(:boxes) && values[:box_count] == 2
+          key.failure('text1 must be exist')
+        end
+      end
+
+      rule(:text0, :text1, :box_count, ) do
+        if values[:box_count] > 2 && (key?(:text0) || key?(:text1))
+          key.failure('text0 and text1 must not be transmitted simultaneously if box_count > 2')
+        end
+      end
+
+      rule(:box_count, :boxes, ) do
+        if key?(:boxes) && values[:box_count] != values[:boxes].count
+          key.failure('box_count != boxes.count')
+        end
+      end
+
+    # box.x + box.width <= template.width
+    # box.y + box.height <= template.height
+    # color != outline_color
+    # текст с заданым шрифтом и размером шрифта должен влезать в бокс
+    #
+    end
+  end
+end
